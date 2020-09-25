a773b86bf8b142d407050d9154dcdbc6c38d338f
Nadia Hansen <nadiahansen.kea@gmail.com>
2020-09-25 10:19:05 +0200

    Initial Commit

---
+++ SwiftUIImages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIImages
+//
+//  Created by Nadia Hansen on 25/09/2020.
+//  Copyright © 2020 Nadia Hansen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
